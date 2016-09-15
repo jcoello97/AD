2560bc2231be016d80e1b34492cb66eeac7659a0
jcoello97 <dam.jcoello@institutoserpis.org>
2016-09-15 10:42:49 +0200

    Añadido metodo leerNumero en PDbPrueba

--- PDbPrueba/PDbPrueba/Program.cs
+++ PDbPrueba/PDbPrueba/Program.cs
@@ -8,12 +8,53 @@ namespace PDbPrueba
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			IDbConnection dbcon;
+			IDbCommand dbcmd;
+			String opcion;
 
-			IDbConnection dbcon = new MySqlConnection ("Database=dbprueba;User ID=root;Password=sistems");
+
+			dbcon = new MySqlConnection ("Database=dbprueba;User ID=root;Password=sistemas");
 			dbcon.Open ();
 
-			dbcon.Close ();
+
+			Console.WriteLine ("*****MENU BASE DE DATOS DBPRUEBA********");
+			Console.WriteLine ("1. Nuevo");
+			Console.WriteLine ("2. Editar");
+			Console.WriteLine ("3. Eliminar");
+			Console.WriteLine ("4. Listar todos");
+			Console.WriteLine ("0. Salir");
+			Console.WriteLine ("Elige una opcion (numero): ");
+
+			opcion = Console.ReadLine ();
+
+		long num = leerNumero (opcion);
+			switch (num) {
+			case 0:
+
+				Console.WriteLine ("Hasta luego ! ");
+				System.Environment.Exit (-1);
+				dbcon.Close ();
+				break;
+
+			}
+
+			dbcmd = dbcon.CreateCommand (); //para ejecutar comandos sql
+
+
+		}
+		private static long leerNumero (String opcion)
+		{
+			while(true)
+			{
+				try
+				{
+					return long.Parse(opcion);
+				}catch
+				{
+					Console.WriteLine ("Solo numeros por favor.");
+				}
+			}
+
 		}
 	}
 }
