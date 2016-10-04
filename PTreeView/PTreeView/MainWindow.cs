9657531e5797b63a53534fab007dffc0c06372e0
jcoello97 <dam.jcoello@institutoserpis.org>
2016-10-04 10:45:58 +0200

    Añadido TreeViewHelper como libreria para PTreeView

--- PTreeView/PTreeView/MainWindow.cs
+++ PTreeView/PTreeView/MainWindow.cs
@@ -2,32 +2,18 @@ using System;
 using Gtk;
 using System.Data;
 using MySql.Data.MySqlClient;
+using Org.InstitutoSerpis.Ad;
 
 public partial class MainWindow: Gtk.Window
 {	
-	protected IDbConnection dbcon = null;
-	protected IDbCommand dbcmd = null;
-	protected IDataReader reader = null;
-	protected ListStore listStore = null;
-
 	public MainWindow (): base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
 
 		string[] columNames = {"id","nombre","precio"};
-		for(int index = 0;index< columNames.Length;index++)
-		{
-			int column = index;
-			treeView.AppendColumn (columNames[column],new CellRendererText(),
-			delegate(TreeViewColumn tree_column, CellRenderer cell, TreeModel tree_model, TreeIter iter) 
-			{
-				CellRendererText cellRendererText = (CellRendererText) cell;
-				object value = tree_model.GetValue(iter,column);
-				cellRendererText.Text = value.ToString();				
-			}
-			);
 
-		}
+		TreeViewHelper.AppendColumns(treeView,columNames);
+
 		ListStore listStore = new ListStore (typeof(int),typeof(string),typeof(decimal));
 		treeView.Model = listStore;
 		listStore.AppendValues (1, "categoria1",20.5m);
--- PTreeView/PTreeView/Program.cs
+++ PTreeView/PTreeView/Program.cs
@@ -1,7 +1,7 @@
 using System;
 using Gtk;
 
-namespace PTreeView
+namespace Org.InstitutoSerpis.Ad
 {
 	class MainClass
 	{
