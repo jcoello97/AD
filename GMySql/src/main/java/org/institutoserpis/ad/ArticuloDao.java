0506b7acc94c024a49dd17336016ee4d54a7b4f3
jcoello97 <dam.jcoello@institutoserpis.org>
2017-02-14 08:19:33 +0100

    Añadido GMysql

---
+++ GMySql/src/main/java/org/institutoserpis/ad/ArticuloDao.java
@@ -0,0 +1,37 @@
+package org.institutoserpis.ad;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+/*
+ * 
+ * */
+
+public class ArticuloDao {
+
+	public static void main(String[] args) throws SQLException{
+		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/dbprueba", "root", "sistemas");
+		/*Statement statement = connection.createStatement();
+		ResultSet resultSet = statement.executeQuery("select * from articulo");*/
+		PreparedStatement preparedStatement = connection.prepareStatement("select * from articulo where id > ?");
+		preparedStatement.setObject(1, Long.parseLong(args[0]));
+		
+		ResultSet resultSet = preparedStatement.executeQuery();
+		System.out.printf("%5s %-30s %10s %9s\n","id","nombre","precio","categoria");
+		while (resultSet.next()) {
+			System.out.printf("%5s %-30s %10s %9s\n"
+					,resultSet.getObject("id")
+					,resultSet.getObject("nombre")
+					,resultSet.getObject("precio")
+					,resultSet.getObject("categoria"));
+		}
+		//statement.close();
+		preparedStatement.close();
+		connection.close();
+
+	}
+
+}
